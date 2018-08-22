13803d283d34cae51a56048d113a05a83d764a0d
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2018-08-22 17:32:08 +0100

    index route

--- middleware/database-calls.js
+++
@@ -1,5 +0,0 @@
-const { Note } = require('../models/notes.js');
-
-Note.findById('5b6ee11f8a888f073c50f18f').then((note) => {
-    console.log(note);
-});
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+var { Note } = require('../models/notes.js');
+
 var expressions = {
   title: 'Express',
 };
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', expressions);
+router.get('/', (req, res, next) => {
+
+  Note.findById('5b6ee11f8a888f073c50f18f').then((note) => {
+    expressions.note = note.text;
+    res.render('index', expressions);
+  });
+
+  
 });
 
 module.exports = router;
